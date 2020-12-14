b85a0618961b3d006b08c0e07db6f2b55d60636b
sinhvien <sinhvien>
2020-12-14 11:23:30 +0700

    tf

---
+++ Lab01/app/src/main/java/a1711061526/lab01/Bai1Activity.java
@@ -0,0 +1,13 @@
+package a1711061526.lab01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Bai1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai4);
+    }
+}
